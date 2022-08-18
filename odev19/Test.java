a29a82a276c321634300a44582d0161b3196267d
Burak Özer <burakozer539@gmail.com>
2022-08-19 00:32:11 +0300

    odev19

--- odev19/Test.java
+++ odev19/Test.java
@@ -8,6 +8,69 @@ import java.sql.SQLException;
 
 public class Test {
 
+	/**
+	 * 
+	 * --1jg customer_id sına göre toplam quantity i veren sorgu select
+	 * customers.customer_id, sum(order_details.unit_price) as sumOfQuantity from
+	 * customers inner join orders on customers.customer_id = orders.customer_id
+	 * inner join order_details on order_details.order_id = orders.order_id group by
+	 * customers.customer_id
+	 * 
+	 * 
+	 * 
+	 * 
+	 * --2g supplier_id sına göre unit_in_stock daki miktarı en fazla ilk 10
+	 * supplier_id veren sorgu
+	 * 
+	 * select supplier_id, sum(units_in_stock) as stok_miktarı from products group
+	 * by supplier_id order by sum(units_in_stock) desc limit 10
+	 * 
+	 * 
+	 * 
+	 * 
+	 * --3g customer ları sehirlere göre saysını veren sorgu
+	 * 
+	 * select city, count(customer_id) from customers group by city order by
+	 * count(customer_id) desc
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * --4jg regiona göre territory sayısını veren sorgu
+	 * 
+	 * select region.region_description, count(territories.territory_id) from region
+	 * inner join territories on region.region_id = territories.region_id group by
+	 * region.region_description
+	 * 
+	 * 
+	 * 
+	 * 
+	 * --5g ship_city şere göre order_id sayilarını veren sorgu
+	 * 
+	 * select ship_city, count(order_id) from orders group by ship_city
+	 * 
+	 * 
+	 * 
+	 * 
+	 * --6jg ship_country göre urun miktarını veren sorgu
+	 * 
+	 * select o.ship_country, sum(od.quantity) from orders as o inner join
+	 * order_details as od on o.order_id = od.order_id group by o.ship_country
+	 * 
+	 * 
+	 * 
+	 * 
+	 * --7jg category_name göre urun_price ortalamasını veren sorgu
+	 * 
+	 * select categories.category_name, avg(products.unit_price) as avg_price from
+	 * categories inner join products on categories.category_id =
+	 * products.category_id group by categories.category_name
+	 * 
+	 * 
+	 * 
+	 */
+
 	public static void main(String[] args) throws Exception {
 
 		Driver.class.forName("org.postgresql.Driver");
